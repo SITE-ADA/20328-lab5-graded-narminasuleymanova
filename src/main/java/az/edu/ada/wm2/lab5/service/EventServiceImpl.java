65f1dea4a1c4830845818f17aee82a36f3e83989
suleymanovanarmina <narmina.suleymanova@kapitalbank.az>
2026-02-26 10:32:03 +0400

    added updateEventPrice method

--- src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
+++ src/main/java/az/edu/ada/wm2/lab5/service/EventServiceImpl.java
@@ -118,7 +118,10 @@ public class EventServiceImpl implements EventService {
 
     @Override
     public Event updateEventPrice(UUID id, BigDecimal newPrice) {
-        return null;
+        Event event = eventRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Event not found with id: " + id));
+        event.setTicketPrice(newPrice);
+        return eventRepository.save(event);
     }
 
 }

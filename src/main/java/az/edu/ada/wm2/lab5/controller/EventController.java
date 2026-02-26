2887a1f206ffffc842a9104cf0b14e83986808bb
suleymanovanarmina <narmina.suleymanova@kapitalbank.az>
2026-02-26 10:44:53 +0400

    added GET /api/events/filter/tag endpoint

--- src/main/java/az/edu/ada/wm2/lab5/controller/EventController.java
+++ src/main/java/az/edu/ada/wm2/lab5/controller/EventController.java
@@ -124,4 +124,15 @@ public class EventController {
     }
   }
 
+  // 9. FILTER BY TAG - GET /api/events/filter/tag
+  @GetMapping("/filter/tag")
+  public ResponseEntity<List<Event>> getEventsByTag(@RequestParam String tag) {
+    try {
+      List<Event> events = eventService.getEventsByTag(tag);
+      return new ResponseEntity<>(events, HttpStatus.OK);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
 }
